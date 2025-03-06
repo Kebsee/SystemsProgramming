b7b7549a6132fce5674161885a14b812390f5040
jake <jakerobb8295@gmail.com>
2025-03-07 00:25:55 +1030

    new

--- ass0a/question08.sh
+++ ass0a/question08.sh
@@ -1,3 +1,2 @@
-mkdir dir8 && touch dir8/balls dir8/secondballs
 mkdir dir8_copy 
 cp -r dir8 dir8_copy
---
+++ ass0a/question09.sh
@@ -0,0 +1,4 @@
+mkdir dir9
+mkdir dir9/dir9_2
+mkdir dir9/dir9_2/dir9_3
+touch dir9/dir9_2/dir9_3/foo
