88cc87d12e097c7afc9c3fd127edf69897173aef
jake <jakerobb8295@gmail.com>
2025-03-21 11:51:08 +1030

    new

--- while.sh
+++ while.sh
@@ -3,6 +3,6 @@ num="2";
 num=$((num))
 str="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-echo"$(str:$num:1)"
 
-# echo "ABCDEFGHIJKLMNOPQRSTUVWXYZ" | tr '[A-Z]' '[B-ZA-B]'
+trans = "$(./create_pattern -k 20 -u )"
+echo $trans
